22d60be753dd737ee0990e2ca876ba19d635ac8e
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-10-08 15:38:57 +0300

    task 1

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -1,15 +1,29 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_ENDPOINT = '/comments';
-const BASE_URL = '';
+const API_ENDPOINT = "/comments";
+const BASE_URL = "https://650b3186dfd73d1fab09c0ad.mockapi.io";
 
 export const commentApi = createApi({
-  reducerPath: 'comments',
+  reducerPath: "comments",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
   }),
-  tagTypes: ['Comments'],
-  endpoints: (builder) => ({}),
+  tagTypes: ["Comments"],
+  endpoints: (builder) => ({
+    getComments: builder.query({
+      query: () => API_ENDPOINT,
+      providesTags: ["Comments"],
+    }),
+
+    addComment: builder.mutation({
+      query: (comment) => ({
+        url: API_ENDPOINT,
+        method: "POST",
+        body: comment,
+      }),
+      invalidatesTags: ["Comments"],
+    }),
+  }),
 });
 
-export const {} = commentApi;
+export const { useGetCommentsQuery, useAddCommentMutation } = commentApi;
